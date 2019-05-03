3b3d513de282e0881b5346084c7ae807cd547cef
Florian Voelkers <florian.voelkers@mail.de>
2019-05-03 14:18:38 +0200

    created model class from example learnopengl.com

---
+++ Flow3D/Flow3D/src/Flow3D/Rendering/Model.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "Mesh.hpp"
+#include "Shader.hpp"
+#include "Flow3D/Math.hpp"
+
+// from https://learnopengl.com/Model-Loading/Model
+
+namespace Flow {
+
+	class Model
+	{
+	public:
+		/* Model Data */
+		std::vector<Texture> textures_loaded;
+		std::vector<Mesh> meshes;
+		std::string directory;
+		bool gammaCorrection;
+
+		/* Functions */
+		// constrcutor, expects a filepath to a 3D model
+		Model(std::string const& path, bool gamma);
+
+		void Draw(Shader shader);
+
+	private:
+		/* Functions */
+		// loads a model with supported assimp extensions from file and stores the resulting meshes in the meshes vector
+		void LoadModel(std::string const& path);
+
+		// processes a node in a recursive fashion. Processes each individual mesh located at the node 
+		// and repeats this process on its children nodes (if any)
+		void ProcessNode(aiNode* node, const aiScene* scene);
+
+		Mesh ProcessMesh(aiMesh* mesh, const aiScene* scene);
+		
+		// checks all material textures of a given type and loads the textures if they're not loaded yet.
+		// the required info is returned as a Texture struct
+		std::vector<Texture> LoadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+
+
+	};
+}
+
