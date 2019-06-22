42c6d0c471904d31f6f814086fbf0dcf516880f2
Florian Voelkers <florian.voelkers@mail.de>
2019-06-22 13:27:45 +0200

    you can add entries to the GOToggler

--- Flow3D/Flow3D/src/Flow3D/ImGui/ImGuiGameObjectTogglerEditor.hpp
+++ Flow3D/Flow3D/src/Flow3D/ImGui/ImGuiGameObjectTogglerEditor.hpp
@@ -14,13 +14,42 @@ namespace Flow {
 		{
 			if (toggler != nullptr)
 			{
-				/*
-				std::string name = toggler->GetGameObjectName();
-				static char nameBuffer[32] = "GameObject";
-				strcpy(nameBuffer, name.c_str());
-				if (ImGui::InputText("Name of GameObject", nameBuffer, IM_ARRAYSIZE(nameBuffer), ImGuiInputTextFlags_CharsNoBlank | ImGuiInputTextFlags_EnterReturnsTrue))
-					toggler->SetGameObjectName(nameBuffer);
-					*/
+				if (ImGui::Button("Add entry", ImVec2(320.0f, 20.0f)))
+					ImGui::OpenPopup("Add Entry to toggle");
+
+				if (ImGui::BeginPopup("Add Entry to toggle"))
+				{
+					static char nameBuffer[32] = "GameObject";
+					ImGui::InputText("Name of GameObject", nameBuffer, IM_ARRAYSIZE(nameBuffer), ImGuiInputTextFlags_CharsNoBlank | ImGuiInputTextFlags_EnterReturnsTrue);
+
+					ImGui::PushItemWidth(200);
+
+					std::vector<std::tuple<Keycode, const char*>>& keyMap = Input::Get().GetKeyMap();
+					static int selectedChar = -1;
+					std::vector<const char*> keysChars;
+					for (unsigned int i = 0; i < keyMap.size(); i++)
+						keysChars.push_back(std::get<1>(keyMap[i]));
+
+					ImGui::Combo("Key", &selectedChar, &keysChars[0], (int)keysChars.size());
+
+					if (ImGui::Button("Add Entry", ImVec2(320.0f, 20.0f)))
+					{
+						GameObject* gameObjectToToggle = Application::Get().GetCurrentScene().FindGameObject(nameBuffer);
+						if (gameObjectToToggle != nullptr)
+						{
+							toggler->AddGameObjectToToggle(std::make_tuple(gameObjectToToggle,
+								nameBuffer, std::get<0>(keyMap[selectedChar])));
+						}
+						else
+						{
+							FLOW_CORE_INFO("Game Object not found");
+						}
+
+						ImGui::CloseCurrentPopup();
+					}
+
+					ImGui::EndPopup();
+				}
 
 				std::vector<std::tuple<GameObject*, std::string, Keycode>>& gameObjectsToToggle = toggler->GetGameObjectsToToggle();
 
@@ -59,7 +88,7 @@ namespace Flow {
 					}
 
 					if (ImGui::Combo("Key", &selectedChar, &keysChars[0], (int)keysChars.size()))
-						std::get<2>(gameObjectsToToggle[j]) = std::get<0>(keyMap[selectedChar]);
+						std::get<2>(gameObjectsToToggle[j]) = std::get<0>(keyMap[selectedChar]);					
 				}
 			}			
 		}
